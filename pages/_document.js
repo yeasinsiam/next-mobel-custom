7894742d337ed8ca4714b9f954270ddd6f197a46
Yeasin Rahman Siam <jhon.siam2004@gmail.com>
2022-06-03 11:06:30 +0600

    Home slider tuch issue fixed

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600&amp;subset=latin-ext&display=swap"
             rel="stylesheet"
@@ -14,17 +15,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap"
             rel="stylesheet"
           />
-
-          {/* <script src="https://code.jquery.com/jquery-3.2.1.min.js" defer />
-          <script src="static/jquery.owl.carousel.min.js" defer />
-          <script src="static/main.js" defer /> */}
-
-          {/* <Script
-            src={}
-            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-            crossorigin="anonymous"
-            // strategy="beforeInteractive"
-          /> */}
         </Head>
         <body>
           <Main />
